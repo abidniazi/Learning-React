5e670b47e5b40a0b04112e671a1230bc8debae4f
Muhammad abid <abidniazi3443@gmail.com>
2026-02-05 18:42:51 +0500

    counter file added

---
+++ 02counter/02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+let [counter,setcounter]=useState(0)
+
+
+
+let increment =()=>{
+  // console.log("clicked",Math.random())
+  // counter+1;
+  if(counter<20){
+  setcounter(counter+1)}
+}
+
+let decrement =()=>{
+  if(counter>0){
+setcounter(counter-1)}
+}
+
+
+
+  return (<>
+ <h1>this is counter {counter}</h1>
+ <button onClick={increment}>Increment Value {counter}</button>
+ <br /><br />
+ <button onClick={decrement}>
+  decrement value {counter}
+ </button>
+</>
+  )
+}
+
+export default App
