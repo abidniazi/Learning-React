14e60540be6abef139d3815cdd21022066687f8d
Muhammad abid <abidniazi3443@gmail.com>
2026-02-07 15:16:47 +0500

    passsword genrator

---
+++ 04PasswordGenrator/04PasswordGenrator/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState,useCallback, useEffect } from 'react'
+
+
+function App() {
+
+  const [length,setLength]=useState(8);
+  const [NumberAllowed,setNumberAllwoed]=useState(false);
+  const [CharAllowed,setCharAllowed]=useState(false);
+  const [password,setpassword]=useState("");
+
+
+  const passwordGenrator=useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(NumberAllowed) str +="0123456789";
+    if(CharAllowed) str +="`!@#$%^&*(){}-_";
+
+  for(let i=1;i<=length;i++){
+    var char= Math.floor(Math.random()*str.length+1);
+    pass += str.charAt(char)
+}
+// console.log(pass)
+setpassword(pass)
+  },[length,NumberAllowed,CharAllowed,setpassword])
+
+useEffect(()=>{passwordGenrator()},[length,NumberAllowed,CharAllowed,passwordGenrator])
+// passwordGenrator()
+
+return(
+  <div className="main">
+    <div className="box">
+   <h1>Password Genrator</h1>
+   <input type="text" className="input" placeholder="Password" value={password} readOnly
+    /> 
+   <button className='copy'>Copy</button> <br />
+<div className="buttom">
+<input type="range" min={8} max={100} value={length} 
+onChange={(e)=>{
+setLength(e.target.value)
+}}
+/>
+<label >Length:{length}</label>
+
+<input type="checkbox" 
+defaultChecked={NumberAllowed}
+onChange={()=>{
+  setNumberAllwoed(prev=>!prev)
+}}
+
+/>
+<label >Numbers</label>
+
+<input type="checkbox" 
+ defaultChecked={CharAllowed}
+ onChange={()=>{
+ setCharAllowed( prev=>!prev) 
+ }}
+  
+ />
+<label >Characters</label>
+</div>
+</div>
+  </div>
+) 
+}
+
+export default App
