b9ae55f6f4c2aecb8ed30437ed3abe38cf7d071c
Muhammad abid <abidniazi3443@gmail.com>
2026-02-11 01:33:19 +0500

    ContextApi

---
+++ practice/practice/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './component/Header'
+function App() {
+
+  return (
+  <>
+  <h1>Practice</h1>
+<Header  value="Hello from App" />
+<Header value="Another Header Value" />
+
+  </>
+  )
+}
+
+export default App
---
+++ practice/practice/src/component/Header.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function Header({value}){
+const [count,setcount]=useState(0)
+return(
+<>
+<h1>Header : {value}</h1>
+<input type="text" />
+
+{/* <button onClick={()=>{setcount(count+1)}} >Click{count}</button> */}
+</>
+
+)
+}
+
+export default Header;
