a277f7d6d5594aa55c16b14165e71d670f6338b4
Muhammad abid <abidniazi3443@gmail.com>
2026-02-19 15:30:59 +0500

    changes

--- practice/practice/src/component/Header.jsx
+++ practice/practice/src/component/Header.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function Header({value,age="22"}){
 const [text,settext]=useState("")
 // useEffect(()=>{
 //     alert(`text entered${text}`)
 // },[text])
+const inputfocus=useRef(null)
+
+const focusinput=()=>{
+    inputfocus.current.focus();
+}
+
 return(
 <>
 <h1>Header : {value} :{age}</h1>
@@ -12,8 +18,14 @@ return(
 value={text}
 onChange={(e)=>settext(e.target.value)}
 />
-
 <p>yoou typed: {text}</p>
+<br /><br />
+
+<input ref={inputfocus} />
+<button onClick={focusinput}>focuus</button>
+<br /><br />
+
+
 
 
 
