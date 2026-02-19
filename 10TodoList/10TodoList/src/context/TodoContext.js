4c36c13240ea96f4f621a9cd8e6f341978aee412
Muhammad abid <abidniazi3443@gmail.com>
2026-02-20 02:21:00 +0500

    todoFunctionality added

--- 10TodoList/10TodoList/src/context/TodoContext.js
+++ 10TodoList/10TodoList/src/context/TodoContext.js
@@ -1,9 +1,22 @@
 import { createContext,useContext } from "react";
 
-const TodoContext = createContext();
+const TodoContext = createContext({
+todos:[
+    {
+        id:1,
+        todo:"todo msg",
+        completed:false
+
+    }
+],
+addTodo:(todo)=>{},
+updateTodo:(id,todo)=>{},
+deleteTodo:(id)=>{},
+toggleComplete:(id)=>{}
+});
 export default TodoContext;
 
-export const useTodoContext = () => {
+export const useTodo = () => {
     
 return useContext(TodoContext);    
 }
